b84cd00cca8077a079450123a640ec12d4649d67
Falko Gebel <falko-gebel@live.de>
2024-08-23 23:21:27 +0200

    - worked on User Login

---
+++ 01_UserLogin/ILogin.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_UserLogin
+{
+    public interface ILogin
+    {
+        public string Login(string loginname, string password);
+        public bool IsLoginValid(string token);
+        public void RequestPasswordReset(string email);
+        public void ResetPassword(string resetRequestNumber);
+    }
+}
