de57dc5fea3039dd8b5d61a029af519df08b9cb1
Falko Gebel <falko-gebel@live.de>
2024-08-24 18:09:06 +0200

    - worked on User Login

--- 01_UserLogin/UserLoginAdministration.cs
+++ 01_UserLogin/UserLoginAdministration.cs
@@ -16,6 +16,11 @@ namespace _01_UserLogin
             throw new NotImplementedException();
         }
 
+        public List<User> GetUsers()
+        {
+            return _users;
+        }
+
         public void Register(string email, string password, string nickname)
         {
             if (string.IsNullOrEmpty(email))
@@ -30,7 +35,20 @@ namespace _01_UserLogin
                 throw new ArgumentException("Invalid email");
             }
 
-            throw new NotImplementedException();
+            DateTime today = DateTime.Now;
+
+            User newUser = new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = email,
+                Nickname = nickname,
+                Confirmed = false,
+                RegistrationDate = today,
+                LastLoginDate = today,
+                LastUpdatedDate = today
+            };
+
+            _users.Add(newUser);
         }
     }
 }
