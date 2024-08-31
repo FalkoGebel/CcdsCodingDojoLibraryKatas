3312939a6a3620546d1ffffd06ceb6a23d1a556d
Falko Gebel <falko-gebel@live.de>
2024-08-31 23:35:14 +0200

    - worked on User Login

--- Tests/UserLoginTests.cs
+++ Tests/UserLoginTests.cs
@@ -238,5 +238,179 @@ namespace Tests
             sut.GetUserForLoginname("Clara").Should().BeEquivalentTo(userClara);
             sut.GetUserForLoginname("Star").Should().BeNull();
         }
+
+        [DataTestMethod]
+        [DataRow("", "")]
+        [DataRow("mail1@mail.com", "")]
+        [DataRow("Peter", "adlkjalsdf")]
+        [DataRow("mail2@mail.com", "adlkjalsdf")]
+        [DataRow("Paul", "2p348dfadf")]
+        [DataRow("mail3@mail.com", "2p348dfadf")]
+        [DataRow("mail4@mail.com", "ABcd01_-1223")]
+        [DataRow("James", "ABcd01_-1223")]
+        [DataRow("mail5@mail.com", "ABcd01_-1223")]
+        [DataRow("mail6@mail.com", "ABcd01_-1223")]
+        [DataRow("Marius", "ABcd01_-1223")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Login_with_invalid_loginname_and_or_password(string loginname, string password)
+        {
+            UserLoginAdministration sut = new();
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.Register("mail2@mail.com", "ABcd01_-1223", "Paul123");
+            registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.Register("mail3@mail.com", "ABcd01_-1223", "");
+            registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.Register("mail4@mail.com", "ABcd01_-1223", "James");
+            sut.Register("mail5@mail.com", "ABcd01_-1223", "");
+            sut.Login(loginname, password);
+        }
+
+        [DataTestMethod]
+        [DataRow("mail1@mail.com", "ABcd01_-1223")]
+        [DataRow("Peter", "ABcd01_-1223")]
+        [DataRow("mail2@mail.com", "ABcd01_-1223")]
+        [DataRow("Paul123", "ABcd01_-1223")]
+        [DataRow("mail3@mail.com", "ABcd01_-1223")]
+        public void Login_with_valid_loginname_and_password(string loginname, string password)
+        {
+            UserLoginAdministration sut = new();
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.Register("mail2@mail.com", "ABcd01_-1223", "Paul123");
+            registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.Register("mail3@mail.com", "ABcd01_-1223", "");
+            registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            string token = sut.Login(loginname, password);
+            token.Should().NotBeNullOrEmpty();
+            token.Length.Should().BeGreaterThanOrEqualTo(10);
+        }
+
+        [TestMethod]
+        public void Give_invalid_token()
+        {
+            UserLoginAdministration sut = new();
+            sut.TokenLifetimeInDays = -10;
+            sut.IsLoginValid("").Should().BeFalse();
+            sut.IsLoginValid("jfll23094jasdlfj20354jlf").Should().BeFalse();
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            string token = sut.Login("Peter", "ABcd01_-1223");
+            sut.IsLoginValid(token).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Give_valid_token()
+        {
+            UserLoginAdministration sut = new();
+            sut.TokenLifetimeInDays = 0;
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            string token = sut.Login("Peter", "ABcd01_-1223");
+            sut.IsLoginValid(token).Should().BeTrue();
+            sut.TokenLifetimeInDays = 10;
+            sut.Register("mail2@mail.com", "ABcd01_-1223", "Pauly_0815");
+            registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            token = sut.Login("Peter", "ABcd01_-1223");
+            sut.IsLoginValid(token).Should().BeTrue();
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("mail1@mail.com")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Request_password_reset_with_invalid_email(string email)
+        {
+            UserLoginAdministration sut = new();
+            sut.RequestPasswordReset(email);
+        }
+
+        [TestMethod]
+        public void Request_password_reset_with_valid_email()
+        {
+            UserLoginAdministration sut = new();
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.RequestPasswordReset(sut.GetUsers()[^1].Email);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("email")]
+        [DataRow("-1200")]
+        [DataRow("0")]
+        [DataRow("invalid_registration_number")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Validate_invalid_reset_request_number(string resetRequestNumber)
+        {
+            UserLoginAdministration.ValidateResetRequestNumber(resetRequestNumber);
+        }
+
+        [DataTestMethod]
+        [DataRow("1")]
+        [DataRow("55")]
+        [DataRow("123478934")]
+        public void Validate_valid_reset_request_number(string resetRequestNumber)
+        {
+            UserLoginAdministration.ValidateResetRequestNumber(resetRequestNumber).Should().Be(uint.Parse(resetRequestNumber));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Reset_password_with_invalid_request_number()
+        {
+            UserLoginAdministration sut = new();
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.RequestPasswordReset(sut.GetUsers()[^1].Email);
+            sut.ResetPassword((-12).ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Reset_password_with_unknown_request_number()
+        {
+            UserLoginAdministration sut = new();
+            sut.Register("mail1@mail.com", "ABcd01_-1223", "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.RequestPasswordReset(sut.GetUsers()[^1].Email);
+
+            Random rnd = new();
+            uint number = 0;
+
+            do
+            {
+                number = (uint)rnd.Next();
+            } while (number == 0 || number == sut.GetUsers()[^1].ResetRequestNumber);
+
+            sut.ResetPassword(number.ToString());
+        }
+
+        [TestMethod]
+        public void Reset_password_with_valid_and_known_request_number()
+        {
+            UserLoginAdministration sut = new();
+            string oldPassword = "ABcd01_-1223";
+            sut.Register("mail1@mail.com", oldPassword, "Peter");
+            string registrationNumber = sut.GetUsers()[^1].RegistrationNumber.ToString();
+            sut.Confirm(registrationNumber);
+            sut.RequestPasswordReset(sut.GetUsers()[^1].Email);
+            string resetRequestNumber = sut.GetUsers()[^1].ResetRequestNumber.ToString();
+            sut.ResetPassword(resetRequestNumber);
+            sut.GetUsers()[^1].Password.Should().NotBeNullOrEmpty();
+            sut.GetUsers()[^1].Password.Should().NotBe(oldPassword);
+        }
     }
 }
