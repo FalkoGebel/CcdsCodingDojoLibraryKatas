a64e72b3a75bf74af03a409c0e6536442dc9d8dd
Falko Gebel <falko-gebel@live.de>
2024-09-15 21:55:49 +0200

    - worked on Folder Stats

--- 03_FolderStats/Folder.cs
+++ 03_FolderStats/Folder.cs
@@ -2,9 +2,11 @@
 {
     public class Folder : IFolder
     {
+        public string ParentPath { get; set; } = string.Empty;
         public string Path { get; set; } = string.Empty;
         public long NumberOfFiles { get; set; }
         public long TotalBytes { get; set; }
         public int Depth { get; set; }
+        public bool Ready { get; set; }
     }
 }
