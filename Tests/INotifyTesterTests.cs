e57245f5fb8ece5520a0011d5662e12c875dd20a
Falko Gebel <falko-gebel@live.de>
2024-09-24 22:21:40 +0200

    - worked on INotify Tester

---
+++ Tests/INotifyTesterTests.cs
@@ -0,0 +1,21 @@
+﻿using _04_INotifyTester;
+
+namespace Tests
+{
+    [TestClass]
+    public class INotifyTesterTests
+    {
+        [TestMethod]
+        public void Test_Valid_Class()
+        {
+            NotificationTester.Verify<ValidTestClass>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Invalid_Class()
+        {
+            NotificationTester.Verify<InvalidTestClass>();
+        }
+    }
+}
